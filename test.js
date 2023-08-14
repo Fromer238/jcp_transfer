598a20ab91d6068562ef3fbf45142ea80eced249
unknown <jay@kinghamm.com>
2023-08-14 11:23:03 +0800

    add size

--- test.js
+++
@@ -1,8 +0,0 @@
-function formatMonth(month) {
-    return month < 10 ? '0' + month : month;
-}
-  
-const currentDate = new Date();
-const currentMonth = formatMonth(currentDate.getMonth() + 1);
-  
-console.log(currentMonth);
